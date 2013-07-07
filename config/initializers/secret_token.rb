759345c1757b8d1f9bd97e3b8d5bca79aeedb59f
Brian Naja <naja.bp@gmail.com>
2013-07-06 20:29:28 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Addspice::Application.config.secret_key_base = '3f73aaa9138af131859614a50db2521782e13b3e8c4840080be34a4d8fc9cd1e2c7259fa6dba61b9394aece18d7ad29679d92d76b676a2f6aa20f2a90f26ca60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Addspice::Application.config.session_store :cookie_store, key: '_addspice_session'
